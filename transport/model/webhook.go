8482ab6814e30d9d91f0a6ae545c09220c6cedf1
Jan Zabloudil <zabl.h@seznam.cz>
2024-05-28 06:23:56 +0200

    feat: add tag name to GithubReleaseWebhook input

--- transport/model/webhook.go
+++ transport/model/webhook.go
@@ -3,12 +3,13 @@ package model
 type GithubReleaseWebhookInput struct {
 	Action  string `json:"action" required:"true"`
 	Release struct {
-		Name string `json:"name" required:"true"`
-		Body string `json:"body"`
+		TagName string `json:"tag_name" required:"true"`
+		Name    string `json:"name"`
+		Body    string `json:"body"`
 	} `json:"release"`
 	Repo struct {
 		// Owner and repository slug of the GitHub repository separated by a slash
 		// (e.g. "owner/repo")
-		Slugs string `json:"full_name"`
+		Slugs string `json:"full_name" required:"true"`
 	} `json:"repository"`
 }
