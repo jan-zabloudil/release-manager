cdb265f68cf2577deddf371ca5404b9e8aa2bae9
Jan Zabloudil <zabl.h@seznam.cz>
2024-06-07 06:15:14 +0200

    feat(github clinet): return error when empty params are provided for url generation

--- github/client.go
+++ github/client.go
@@ -227,7 +227,7 @@ func (c *Client) updateRelease(ctx context.Context, tkn string, repo svcmodel.Gi
 
 func (c *Client) GenerateRepoURL(ownerSlug, repoSlug string) (url.URL, error) {
 	if ownerSlug == "" || repoSlug == "" {
-		return url.URL{}, nil
+		return url.URL{}, errors.New("empty owner or repo slug")
 	}
 
 	rawURL := fmt.Sprintf("https://github.com/%s/%s", ownerSlug, repoSlug)
@@ -241,7 +241,7 @@ func (c *Client) GenerateRepoURL(ownerSlug, repoSlug string) (url.URL, error) {
 
 func (c *Client) GenerateGitTagURL(ownerSlug, repoSlug, tagName string) (url.URL, error) {
 	if tagName == "" || ownerSlug == "" || repoSlug == "" {
-		return url.URL{}, nil
+		return url.URL{}, errors.New("empty tag name, owner or repo slug")
 	}
 
 	rawURL := fmt.Sprintf("https://github.com/%s/%s/releases/tag/%s", ownerSlug, repoSlug, tagName)
