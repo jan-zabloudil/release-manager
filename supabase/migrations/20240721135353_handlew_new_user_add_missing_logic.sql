e56eba18618ef2497e1cbde34deb9d7f163857da
Jan Zabloudil <zabl.h@seznam.cz>
2024-07-21 16:12:53 +0200

    fix(handle_new_user postgres function): add missing funcionality for adding user to projects

---
+++ supabase/migrations/20240721135353_handlew_new_user_add_missing_logic.sql
@@ -0,0 +1,31 @@
+-- fixes function added in 20240720124825_handle_user_nullable_fields.sql
+-- previous function was not handling the case where the user has accepted an invitation before signing up
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+    RETURNS trigger
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    SET search_path TO 'public'
+AS $function$
+BEGIN
+    INSERT INTO public.users (id, email, name, avatar_url, created_at, updated_at)
+    VALUES (
+               new.id,
+               new.email,
+               COALESCE(new.raw_user_meta_data ->> 'name', ''),
+               COALESCE(new.raw_user_meta_data ->> 'picture', ''),
+               new.created_at,
+               new.updated_at
+           );
+
+    -- If user has accepted an invitation(s) before signing up, add them to the project(s)
+    INSERT INTO public.project_members (user_id, project_id, project_role, created_at, updated_at)
+    SELECT NEW.id, project_id, project_role, NEW.created_at, NEW.updated_at
+    FROM public.project_invitations
+    WHERE email = NEW.email AND status = 'accepted_awaiting_registration';
+
+    DELETE FROM public.project_invitations
+    WHERE email = NEW.email AND status = 'accepted_awaiting_registration';
+
+    RETURN new;
+END;
+$function$;
