b02977b84d37f90c854ec2ef15125c01e6b5d03c
Jan Zabloudil <zabl.h@seznam.cz>
2024-08-21 17:44:30 +0200

    refactor(service): rename function params for clarity

--- service/settings.go
+++ service/settings.go
@@ -35,13 +35,13 @@ func (s *SettingsService) Get(ctx context.Context, authUserID uuid.UUID) (model.
 	return settings, nil
 }
 
-func (s *SettingsService) Update(ctx context.Context, u model.UpdateSettingsInput, authUserID uuid.UUID) (model.Settings, error) {
+func (s *SettingsService) Update(ctx context.Context, input model.UpdateSettingsInput, authUserID uuid.UUID) (model.Settings, error) {
 	if err := s.authGuard.AuthorizeUserRoleAdmin(ctx, authUserID); err != nil {
 		return model.Settings{}, fmt.Errorf("authorizing user role: %w", err)
 	}
 
 	settings, err := s.repository.Update(ctx, func(s model.Settings) (model.Settings, error) {
-		if err := s.Update(u); err != nil {
+		if err := s.Update(input); err != nil {
 			return model.Settings{}, svcerrors.NewSettingsUnprocessableError().Wrap(err).WithMessage(err.Error())
 		}
 
